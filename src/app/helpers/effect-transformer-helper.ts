179e04ded22dd3bbd1e893fb20db9045df42347f
terencequ <therealterencequ@gmail.com>
2025-04-25 18:06:56 +1000

    Fix effect display

--- src/app/helpers/effect-transformer-helper.ts
+++ src/app/helpers/effect-transformer-helper.ts
@@ -2,5 +2,5 @@ import {EffectType} from '../models/effect-type';
 
 export function getEffectTransformerDictionaryAsArray(dictionary: {[key in EffectType]?: EffectType}): {from: EffectType, to: EffectType}[] {
   const effectTypes = Object.keys(dictionary).map(key => Number(key) as EffectType);
-  return effectTypes.filter(effectType => dictionary[effectType]).map((effectType) => ({from: effectType, to: dictionary[effectType]!}));
+  return effectTypes.filter(effectType => dictionary[effectType] !== undefined).map((effectType) => ({from: effectType, to: dictionary[effectType]!}));
 }
